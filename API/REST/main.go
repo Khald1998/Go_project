2e8258898774c599b4ca68c3c270af4446e9fa40
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-01-21 19:06:00 +0300

    -

--- API/REST/main.go
+++ API/REST/main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-// event represents data about a record album.
+// event represents data about a record event.
 type event struct {
-	ID  int `json:"ID"`
-	X   int `json:"X"`
-	Y   int `json:"Y"`
-	RES int `json:"RES"`
+	ID  int `json:"id"`
+	X   int `json:"x"`
+	Y   int `json:"y"`
+	RES int `json:"res"`
 }
 
-// events slice to seed record album data.
+// events slice to seed record event data.
 var events = []event{
 	{ID: 0, X: 0, Y: 0, RES: 0},
 	{ID: 1, X: 1, Y: 1, RES: 2},
@@ -25,32 +27,53 @@ func main() {
 	router := gin.Default()
 	router.GET("/events/show", getEvents)
 	router.GET("/events/show/:ID", getEventsByID)
-	router.POST("/events/add", addEvents)
+	router.POST("/events/addJ", addJEvents)
+	router.POST("/events/addQ", addQEvents)
+	router.PUT("/events/updata", updateEvent)
+	router.DELETE("/events/delete", deleteEvent)
 
 	router.Run("localhost:8080")
 }
 
 // getEvents responds with the list of all events as JSON.
 func getEvents(c *gin.Context) {
+
 	c.IndentedJSON(http.StatusOK, events)
 }
 
 // addEvents adds an event from JSON received in the request body.
-func addEvents(c *gin.Context) {
+func addJEvents(c *gin.Context) {
 	var newEvent event
-	// ID, _ := strconv.Atoi(events[len(events)-1].ID)
-	// ID = ID + 1
-	// X, _ := strconv.Atoi(c.Param("X"))
-	// Y, _ := strconv.Atoi(c.Param("X"))
-	// RES := X + Y
+	decoder := json.NewDecoder(c.Request.Body)
 
-	// Call BindJSON to bind the received JSON to
-	// newEvent.
-	if err := c.BindJSON(&newEvent); err != nil {
-		return
+	err := decoder.Decode(&newEvent)
+	if err != nil {
+		panic(err)
 	}
-	// newEvent.ID = strconv.Itoa(ID)
-	// newEvent.RES = strconv.Itoa(RES)
+	newEvent.RES = newEvent.X + newEvent.Y
+	// // Call BindJSON to bind the received JSON to
+	// // newEvent.
+
+	ID := events[len(events)-1].ID
+	newEvent.ID = ID + 1
+
+	// Add the new event to the slice.
+	events = append(events, newEvent)
+	c.IndentedJSON(http.StatusCreated, newEvent)
+}
+
+// addEvents adds an event from query received in URL.
+func addQEvents(c *gin.Context) {
+	var newEvent event
+
+	ID := events[len(events)-1].ID
+	X, _ := strconv.Atoi(c.Query("X"))
+	Y, _ := strconv.Atoi(c.Query("Y"))
+	RES := X + Y
+	newEvent.ID = ID + 1
+	newEvent.RES = RES
+	newEvent.X = X
+	newEvent.Y = Y
 	// Add the new event to the slice.
 	events = append(events, newEvent)
 	c.IndentedJSON(http.StatusCreated, newEvent)
@@ -71,4 +94,88 @@ func getEventsByID(c *gin.Context) {
 		}
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "event not found"})
+}
+
+// func updateEvent(c *gin.Context) {
+// 	ID := c.Query("ID")
+// 	eventID, _ := strconv.Atoi(ID)
+// 	var updatedEvent event
+
+// 	reqBody, err := ioutil.ReadAll(c.Request.Body)
+// 	if err != nil {
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Kindly enter data with the event only in order to update"})
+// 	}
+// 	json.Unmarshal(reqBody, &updatedEvent)
+
+// 	for i, singleEvent := range events {
+// 		if singleEvent.ID == eventID {
+// 			singleEvent.ID = updatedEvent.ID
+// 			singleEvent.X = updatedEvent.X
+// 			singleEvent.Y = updatedEvent.Y
+// 			singleEvent.RES = updatedEvent.Y + updatedEvent.X
+
+// 			events = append(events[:i], singleEvent)
+// 			c.IndentedJSON(http.StatusCreated, singleEvent)
+// 		}
+// 	}
+// }
+
+func updateEvent(c *gin.Context) {
+
+	var isThere bool = false
+	var updatedEvent event
+	decoder := json.NewDecoder(c.Request.Body)
+
+	err := decoder.Decode(&updatedEvent)
+	if err != nil {
+		panic(err)
+	}
+	updatedEvent.RES = updatedEvent.X + updatedEvent.Y
+	eventID := updatedEvent.ID
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			isThere = true
+			singleEvent.ID = updatedEvent.ID
+			singleEvent.X = updatedEvent.X
+			singleEvent.Y = updatedEvent.Y
+			singleEvent.RES = updatedEvent.Y + updatedEvent.X
+
+			temp := events[i+1:]
+			events = append(events[:i], singleEvent)
+			events = append(events, temp...)
+			c.IndentedJSON(http.StatusCreated, singleEvent)
+		}
+	}
+	if !isThere {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "event not found"})
+
+	}
+
+}
+
+func deleteEvent(c *gin.Context) {
+	var isThere bool = false
+	var updatedEvent event
+	decoder := json.NewDecoder(c.Request.Body)
+
+	err := decoder.Decode(&updatedEvent)
+	if err != nil {
+		panic(err)
+	}
+	updatedEvent.RES = updatedEvent.X + updatedEvent.Y
+	eventID := updatedEvent.ID
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			events = append(events[:i], events[i+1:]...)
+			isThere = true
+			fmt.Fprintf(c.Writer, "The event with ID %v has been deleted successfully", eventID)
+
+		}
+	}
+	if !isThere {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "event not found"})
+
+	}
 }
